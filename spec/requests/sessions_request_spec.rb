2870debe01346479935d075f9e950bdad10df0c5
touhouota <touhouota.register@gmail.com>
2020-09-26 22:03:44 +0900

    sessionコントローラ等をgenerate

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
     it "returns http success" do
-      get login_path
+      get "/sessions/new"
       expect(response).to have_http_status(:success)
     end
   end
